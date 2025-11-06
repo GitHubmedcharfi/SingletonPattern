65d0f0f347d9f0cb0d2cabe91f2c0b062e6ec646
GitHubmedcharfi <118690627+GitHubmedcharfi@users.noreply.github.com>
2025-11-06 16:54:53 +0100

    Delete bank_system directory

--- bank_system/src/tn/banquetunisie/bankingsystem/payment/PaymentMethod.java
+++
@@ -1,5 +0,0 @@
-package tn.banquetunisie.bankingsystem.payment;
-
-public interface PaymentMethod {
-    void processPayment(double amount);
-}
--- bank_system/src/tn/banquetunisie/bankingsystem/payment/WireTransfer.java
+++
@@ -1,9 +0,0 @@
-package tn.banquetunisie.bankingsystem.payment;
-
-public class WireTransfer implements PaymentMethod {
-
-    @Override
-    public void processPayment(double amount) {
-        System.out.println("Transferred $" + amount + " via Wire Transfer.");
-    }
-}
